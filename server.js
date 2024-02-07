a777774f702ad52cda560be50ff6fe8642dfbf11
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-06 21:28:28 -0500

    changes for git health api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { checkIfPayloadIsEmpty } = require('./service');
 
 var app = express();
 app.use(bodyParser.json());
@@ -9,6 +10,35 @@ app.use('/', (req, res, next) => {
     res.header('Cache-Control', 'no-cache');
     next();
 });
+
+app.use('/healthz', (req, res, next) => {
+    if (req.method !== 'GET') {
+        res.status(405).send();
+    }
+    next();
+});
+
+app.get('/healthz', async function (req, res) {
+    checkIfPayloadIsEmpty(req, res);
+    (async () => {
+        try {
+            await database.sequelize.authenticate();
+            (async () => {
+                await User.sync({ force: true });
+                // Table created
+                const users = await User.findAll();
+                console.log(users);
+
+            })();
+            console.log('Connection has been established successfully.');
+            res.status(200).send();
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+            res.status(503).send();
+        }
+    })();
+});
+
 app.use('/', (req, res) => {
     res.status(404).send();
 });
---
+++ service.js
@@ -0,0 +1,12 @@
+// Description: This file contains the middleware functions for the service routes.
+function checkIfPayloadIsEmpty(req, res) {
+    console.log(req.route,req.method);
+    if (Object.keys(req.body).length !== 0) {
+        res.status(400).send();
+    }
+    if (Object.keys(req.query).length > 0) {
+        res.status(400).send();
+    }
+}
+
+module.exports = { checkIfPayloadIsEmpty};
