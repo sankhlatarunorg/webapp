0a5660ec383892bf98e0ef0a1cdf22a388235f3c
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-15 13:26:10 -0500

    changesto sync

--- test/integration-test.mjs
+++ test/integration-test.mjs
@@ -2,6 +2,7 @@ const chai = await import('chai');
 const expect = chai.expect;
 import supertest from 'supertest';
 import { faker } from '@faker-js/faker';
+const User = await import('./../Model').users;
 
 const api = supertest('http://localhost:3000');
 
@@ -23,6 +24,16 @@ describe(' Api Integration Test', function () {
       last_name: 'Sankhla'
     }
     console.log(userData);
+    console.log('starting the sync');
+    (async () => {
+      await User.sync({ force: true });
+      // Table created
+      const users = await User.findAll();
+      console.log("Users:")
+      console.log(users);
+
+    })();
+    console.log('ending the sync');
 
     const healthz = await api.get('/healthz');
     console.log("successfully ran health check");
@@ -48,7 +59,7 @@ describe(' Api Integration Test', function () {
     userCredsData.id = response.body.id;
     userCredsData.account_created = response.body.account_created;
     userCredsData.account_updated = response.body.account_updated;
- 
+
     response = await api.get('/v1/user/self').auth(userCredsData.username, userCredsData.password);
     console.log(response.status);
     console.log(response.body);
@@ -62,7 +73,7 @@ describe(' Api Integration Test', function () {
     expect(response.body.username).to.equal(userCredsData.username);
     expect(response.body.first_name).to.equal(userCredsData.first_name);
     expect(response.body.last_name).to.equal(userCredsData.last_name);
-    expect(response.body.id).to.equal(userCredsData.id);  
+    expect(response.body.id).to.equal(userCredsData.id);
     expect(response.body.account_created).to.equal(userCredsData.account_created);
     expect(response.body.account_updated).to.equal(userCredsData.account_updated);
   });
@@ -72,13 +83,14 @@ describe(' Api Integration Test', function () {
     userCredsData.first_name += 'Updated';
     let response = await api.put('/v1/user/self')
       .auth(userCredsData.username, userCredsData.password)
-      .send({ 
-        first_name: userCredsData.first_name, 
-        last_name: userCredsData.last_name, 
-        password: userCredsData.password, 
-        username: userCredsData.username });
-    console.log(response.status); 
-    console.log(response.body); 
+      .send({
+        first_name: userCredsData.first_name,
+        last_name: userCredsData.last_name,
+        password: userCredsData.password,
+        username: userCredsData.username
+      });
+    console.log(response.status);
+    console.log(response.body);
     expect(response.status).to.equal(204);
 
     response = await api.get('/v1/user/self').auth(userCredsData.username, userCredsData.password);
@@ -94,6 +106,6 @@ describe(' Api Integration Test', function () {
     expect(response.body.username).to.equal(userCredsData.username);
     expect(response.body.first_name).to.equal(userCredsData.first_name);
     expect(response.body.last_name).to.equal(userCredsData.last_name);
-    expect(response.body.id).to.equal(userCredsData.id); 
+    expect(response.body.id).to.equal(userCredsData.id);
   });
 });
