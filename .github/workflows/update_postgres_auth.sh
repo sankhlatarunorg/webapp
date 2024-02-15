c86fa071fb65c01575abb88826831c71eff51b7f
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-15 12:50:33 -0500

    update shell script

--- .github/workflows/update_postgres_auth.sh
+++ .github/workflows/update_postgres_auth.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Set the path to your pg_hba.conf file
 PG_HBA_CONF="/var/lib/pgsql/data/pg_hba.conf"
 
-# Change the authentication method to "md5" for password authentication
 sed -i 's/ident/md5/g' $PG_HBA_CONF
 
-# Restart PostgreSQL to apply changes
-systemctl restart postgresql  # Adjust the command based on your system
+systemctl restart postgresql
