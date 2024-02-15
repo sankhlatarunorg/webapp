ef8194c8368401583a481a00eb692cdff325212b
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-15 14:31:21 -0500

    update test

--- test/integration-test.mjs
+++ test/integration-test.mjs
@@ -16,17 +16,7 @@ describe(' Api Integration Test', function () {
     account_created: '',
     account_updated: ''
   };
-  // console.log('starting the sync');
-  // const User = database.users;
-  // (async () => {
-  //   await User.sync({ force: true });
-  //   // Table created
-  //   const users = await User.findAll();
-  //   console.log("Users:")
-  //   console.log(users);
 
-  // })();
-  // console.log('ending the sync');
   it('Create User Test', async function () {
     const userData = {
       username: faker.internet.email(),
@@ -35,6 +25,17 @@ describe(' Api Integration Test', function () {
       last_name: 'Sankhla'
     }
     console.log(userData);
+    console.log('starting the sync');
+    const User = database.users;
+    (async () => {
+      await User.sync({ force: true });
+      // Table created
+      const users = await User.findAll();
+      console.log("Users:")
+      console.log(users);
+  
+    })();
+    console.log('ending the sync');
     // try {
     //   console.log('Checking database connection...');
     //   const sequelize= database.sequelize;
@@ -96,6 +97,17 @@ describe(' Api Integration Test', function () {
 
 
   it('Update the User Test', async function () {
+    console.log('starting the sync');
+    const User = database.users;
+    (async () => {
+      await User.sync({ force: true });
+      // Table created
+      const users = await User.findAll();
+      console.log("Users:")
+      console.log(users);
+  
+    })();
+    console.log('ending the sync');
     userCredsData.first_name += 'Updated';
     let response = await api.put('/v1/user/self')
       .auth(userCredsData.username, userCredsData.password)
