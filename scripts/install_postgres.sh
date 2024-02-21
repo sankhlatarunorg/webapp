c2a3459910454221424752fb4efe8e11d47fcec6
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-20 21:36:32 -0500

    added shell script to setup csye user

--- scripts/install_postgres.sh
+++ scripts/install_postgres.sh
@@ -5,3 +5,15 @@ sudo dnf -y install postgresql-server
 sudo postgresql-setup --initdb
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
+sudo yum install -y nodejs gcc-c++ make
+sudo dnf module -y reset nodejs
+sudo dnf module -y  enable nodejs:16
+sudo yum install -y nodejs 
+sudo dnf install -y git
+sudo yum -y install zip unzip
+cd ..
+cd /tmp/webapp
+sudo unzip webapp.zip
+cd webapp
+cd myapp
+ls
