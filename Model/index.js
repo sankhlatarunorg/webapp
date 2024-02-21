de796e99f6cbcceca8d71a193058f422d8dbf213
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-21 13:07:27 -0500

    changes in sequelize

--- Model/index.js
+++ Model/index.js
@@ -3,12 +3,12 @@ const {Sequelize,DataTypes} = require('sequelize')
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME || 'postgres',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || 'postgres',
     {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST || 'localhost',
+        dialect: process.env.DB_DIALECT || 'postgres',
         logging: false
     }
 );
