2131d3db2becfa70698c115f23306ee5f3e94f14
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-02-24 20:55:28 -0500

    changes for pgsql

--- scripts/install_postgres.sh
+++ scripts/install_postgres.sh
@@ -2,8 +2,11 @@
 sudo dnf module list postgresql
 sudo yum install -y postgresql-server postgresql-contrib
 sudo postgresql-setup --initdb
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
+sudo systemctl start postgresql.service
+sudo systemctl status postgresql.service
+pg_isready
+sudo su - postgres -c "psql --echo-all -U postgres -d postgres --command \"ALTER USER postgres WITH PASSWORD 'password';\""
+sudo systemctl restart postgresql
 sudo yum install -y nodejs gcc-c++ make
 sudo dnf module -y reset nodejs
 sudo dnf module -y  enable nodejs:16
@@ -20,6 +23,8 @@ ls
 sudo npm install -y
 ls
 sudo cp -f /tmp/webapp/webapp/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
+sudo systemctl stop postgresql
+sudo systemctl start postgresql  
 sudo chown -R csye6225:csye6225 ./
 # sudo postgresql-setup --initdb
 # sudo systemctl start postgresql
